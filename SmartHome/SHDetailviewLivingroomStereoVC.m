6adf2ea84bd29061927714a4e7345e0fcce7ec2f
Clara Lange <c.lange@salesperformer.net>
2013-07-18 00:37:15 +0200

    - added volume slider (without storage of value) for stereo in living room

--- SmartHome/SHDetailviewLivingroomStereoVC.m
+++ SmartHome/SHDetailviewLivingroomStereoVC.m
@@ -10,6 +10,11 @@
 
 @interface SHDetailviewLivingroomStereoVC ()
 
+@property (weak, nonatomic) IBOutlet UISlider *sliderVolume;
+@property (weak, nonatomic) IBOutlet UILabel *labelVolume;
+@property (nonatomic, assign) float currentVolumeValue;
+- (IBAction)sliderVolumeChange:(UISlider *)sender;
+
 @end
 
 @implementation SHDetailviewLivingroomStereoVC
@@ -27,6 +32,14 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    // storage of volume
+    //NSUserDefaults * standardUserDefaults = [NSUserDefaults standardUserDefaults];
+    //[standardUserDefaults setFloat:self.currentVolumeValue forKey:@"floatVolKey"];
+    //float currentVol = [standardUserDefaults floatForKey:@"floatVolKey"];
+    /*[[NSUserDefaults standardUserDefaults] setValue:[NSNumber numberWithFloat:self.currentVolumeValue] forKey:@"currentVolumeValue"];
+    float currentVol = [[[NSUserDefaults standardUserDefaults] valueForKey:@"currentVolumeValue"] floatValue];*/
+    //self.sliderVolume.value = currentVol;
 }
 
 - (void)didReceiveMemoryWarning
@@ -45,4 +58,12 @@
     [self.presentingViewController.presentingViewController dismissViewControllerAnimated:NO completion:nil];
     // TODO: wirklich alle Views dismissed?
 }
+
+- (IBAction)sliderVolumeChange:(UISlider *)sender
+{
+    self.labelVolume.text = NULL;
+    self.currentVolumeValue = [sender value] * 100;
+    self.labelVolume.text = [NSString stringWithFormat:@"%.1f", self.currentVolumeValue];
+}
+
 @end
